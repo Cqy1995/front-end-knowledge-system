64b2c174d533dd9bce6e5e07897df1a9b9a6a714
CQY1125 <596654003@qq.com>
2021-07-06 10:12:57 +0800

    update promise

--- 编码能力培养题/promise.js
+++ 编码能力培养题/promise.js
@@ -43,3 +43,32 @@ for (let index = 0; index < arr.length; index++) {
         }
     }
 }
+
+
+
+Promise.resolve().then(()=>{
+    console.log(1);
+}).catch(()=>{
+    console.log(2);
+}).then(()=>{
+    console.log(3);
+})
+
+Promise.resolve().then(()=>{
+    console.log(1);
+    throw new Error('error1')
+}).catch(()=>{
+    console.log(2);
+}).then(()=>{
+    console.log(3);
+})
+
+Promise.resolve().then(()=>{
+    console.log(1);
+    throw new Error('error1')
+}).catch(()=>{
+    console.log(2);
+}).catch(()=>{
+    console.log(3);
+})
+
