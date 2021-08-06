21dcb50986e3056351aa00b57f5527ba30116ec0
CQY1125 <596654003@qq.com>
2021-08-06 18:23:31 +0800

    继续 数据结构与算法 树与二叉树

--- 编码能力培养题/dataStructure.js
+++ 编码能力培养题/dataStructure.js
@@ -147,3 +147,200 @@ m.delete('b');
 // m.clear();清空所有键值对
 //改
 m.set('a','aaba')
+
+/**
+ * 树
+*/
+const tree = {
+    val: 'a',
+    children: [
+        {
+            val: 'b',
+            children: [
+                {
+                    val: 'e',
+                    children: []
+                },
+                {
+                    val: 'f',
+                    children: []
+                }
+            ]
+        },
+        {
+            val: 'c',
+            children: [
+                {
+                    val: 'g',
+                    children: []
+                },
+                {
+                    val: 'h',
+                    children: []
+                }
+            ]
+        }
+    ]
+};
+//深度优先遍历
+const dfs = (root)=>{
+    console.log(root.val);
+    root.children.forEach(dfs);
+}
+dfs(tree)
+//广度优先遍历
+const bfs = (root) => {
+    const q = [root];
+    while (q.length) {
+        const n = q.shift();
+        console.log(n.val);
+        n.children.forEach(element => {
+            q.push(element)
+        });
+    }
+}
+bfs(tree)
+
+/**
+ * 二叉树
+*/
+const bt = {
+    val: 1,
+    left: {
+        val: 2,
+        left: {
+            val: 4,
+            left: null,
+            right: null
+        },
+        right: {
+            val: 5,
+            left: null,
+            right: null
+        }
+    },
+    right: {
+        val: 3,
+        left: {
+            val: 6,
+            left: null,
+            right: null
+        },
+        right: {
+            val: 7,
+            left: null,
+            right: null
+        }
+    }
+}
+//递归版
+//先序遍历
+const preorder = (root) => {
+    if (!root) {
+        return
+    }
+    console.log(root.val);
+    preorder(root.left);
+    preorder(root.right)
+}
+preorder(bt)
+//中序遍历
+const inorder = (root) => {
+    if (!root) {
+        return
+    }
+    inorder(root.left);
+    console.log(root.val);
+    inorder(root.right)
+}
+inorder(bt)
+//后序遍历
+const postorder = (root) => {
+    if (!root) {
+        return
+    }
+    postorder(root.left);
+    postorder(root.right);
+    console.log(root.val);
+}
+postorder(bt)
+
+// 非递归版
+/**
+ * 先序遍历
+ * 思路:
+ * 创建一个栈,默认是当前节点
+ * 弹窗栈顶,输出
+ * 并把右子树和左子树推入栈中
+ * 注意:先序遍历输出是左中右,所以右子树先入栈
+ * 进而根据栈是否为空就可以循环了
+*/
+const preorderFdg = (root) => {
+    if (!root) { return }
+    const stack = [root];
+    while (stack.length > 0) {
+        const n = stack.pop();
+        console.log(n.val);
+        if (n.right) {
+            stack.push(n.right)
+        }
+        if (n.left) {
+            stack.push(n.left)
+        }
+    }
+}
+preorderFdg(bt)
+/**
+ * 中序遍历
+ * 创建一个栈,和一个指针,指针初始化为根节点
+ * 有指针的时候就把指针左节点先推到栈中
+ * 然后执行根节点
+ * 所有左节点都推完后,把右节点推到栈中,
+ * 直到栈清空为止
+ * */ 
+const inorderFdg = (root) => {
+    if (!root) {
+        return
+    }
+    const stack = [];
+    let p = root;
+    while (stack.length || p) {
+        while (p) {
+            stack.push(p)
+            p = p.left
+        }
+        const n = stack.pop();
+        console.log(n.val);
+        p = n.right
+    }
+}
+inorderFdg(bt)
+/**
+ * 后序遍历
+ * 思路和先序遍历一样
+ * 只不过在执行的时候,把所有推到一个新栈中,
+ * 然后新栈反向输出
+ * */
+const postorderFdg = (root) => {
+    if (!root) {
+        return
+    }
+    const  stack = [root];
+    const outputstack = [];
+    while(stack.length){
+        const n = stack.pop();
+        outputstack.push(n)
+        if (n.left) {
+            stack.push(n.left)
+        }
+        if (n.right) {
+            stack.push(n.right)
+        }
+    }
+    while(outputstack.length){
+        const n = outputstack.pop()
+        console.log(n.val);
+    }
+}
+postorderFdg(bt)
+
